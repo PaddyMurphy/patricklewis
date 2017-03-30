da37601a7c748f5eaf718f23c5554a296494f44d
PaddyMurphy <patrickblewis@gmail.com>
2017-03-30 11:54:24 -0500

    slideshow

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -5,6 +5,25 @@
         <h1 class="title">Patrick Lewis</h1>
         <span class="tagline">User Experience Developer</span>
       </div>
+
+      <ul class="photo-slideshow">
+        <li class="photo-slide photo-slide--start">
+          <span class="photo-slideshow-img" v-bind:style="{ backgroundImage: 'url(' + startPhoto.src + ')' }"></span>
+          <div class="photo-title-container">
+            <h3>{{startPhoto.title}}</h3>
+          </div>
+        </li>
+
+        <lazy-background v-for="photo in photos"
+          class="photo-slide"
+          :image-source="photo.src"
+          :image-title="photo.title"
+          :key="photo.src"
+          loadingImage="/static/img/blank.png"
+          errorImage="/static/img/blank.png">
+        </lazy-background>
+      </ul>
+
     </div>
     <div class="content">
 
@@ -58,15 +77,42 @@
 <script>
 import Stickyheader from 'sticky-header';
 import Smoothscroll from 'smooth-scroll';
+import LazyBackgroundImages from '@/components/VueLazyBackgroundImage';
 
 export default {
   name: 'container',
   data () {
     return {
-      msg: 'Portfolio'
+      startPhoto: {
+        'title': 'Mt Shucksan, Washington',
+        'src': '/static/img/mt-shuksan.jpg'
+      },
+      photos: [
+        {
+          'title': 'Jacobs Well, Texas',
+          'src': '/static/img/jacobs-well.jpg'
+        },
+        {
+          'title': 'Bugaboo Spire, Canada',
+          'src': '/static/img/bugaboo-spire.jpg'
+        },
+        {
+          'title': 'Snowpatch Spire, Canada',
+          'src': '/static/img/bugaboo-spire-2.jpg'
+        },
+        {
+          'title': 'Devils River, Texas',
+          'src': '/static/img/devils-river.jpg'
+        },
+        {
+          'title': 'America, USA',
+          'src': '/static/img/america.jpg'
+        }
+      ]
     }
   },
   mounted: function () {
+    // var that = this;
     var hero = this.$el.querySelector('.hero');
     var nav = this.$el.querySelector('.nav');
     // stick header to top on scroll up
@@ -78,6 +124,28 @@ export default {
       'easing': 'easeOutQuad',
       'offset': 200
     });
+  },
+  components: {
+    'lazy-background': LazyBackgroundImages
+  },
+  methods: {
+    cyclePhoto: function () {
+      // var img = this.$el.querySelector('.photo');
+      // reset currentPhoto at end
+      // this.currentPhotoPath = 'url(' + this.photos[this.currentPhoto].src + ')';
+
+      // img.style['background-image'] = 'url(' + this.photos[this.currentPhoto].src + ')';
+    }
+    // incrementPhoto: function () {
+    //   console.log(this.currentPhoto === this.photos.length);
+    //   if (this.currentPhoto === this.photos.length) {
+    //     this.currentPhoto = 0;
+    //   } else {
+    //     this.currentPhoto = this.currentPhoto + 1;
+    //   }
+
+    //   console.log('currentPhoto ' + this.currentPhoto);
+    // }
   }
 }
 </script>
---
+++ src/components/VueLazyBackgroundImage.vue
@@ -0,0 +1,117 @@
+<!-- https://github.com/darrynten/vue-lazy-background-images -->
+<template>
+  <li>
+    <span
+      class="photo-slideshow-img"
+      :class="[imageClass, imageState]"
+      :style="computedStyle"
+      :data-width="imageWidth"
+      :data-height="imageHeight"
+      :data-state="imageState">
+    </span>
+    <div class="photo-title-container">
+      <h3>{{imageTitle}}</h3>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    imageSource: {
+      type: String,
+      required: true
+    },
+    imageClass: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    loadingImage: {
+      type: String,
+      required: true
+    },
+    errorImage: {
+      type: String,
+      required: true
+    },
+    imageErrorCallback: {
+      type: Function,
+      required: false,
+      default: function () {}
+    },
+    imageSuccessCallback: {
+      type: Function,
+      required: false,
+      default: function () {}
+    },
+    backgroundSize: {
+      type: String,
+      required: false,
+      default: 'cover'
+    },
+    imageTitle: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    delay: {
+      type: Number,
+      required: false,
+      default: 2000
+    }
+  },
+  data () {
+    return {
+      imageWidth: 0,
+      imageHeight: 0,
+      imageState: 'loading',
+      asyncImage: new Image()
+    }
+  },
+  computed: {
+    computedStyle () {
+      if (this.imageState === 'loading') {
+        return 'background-image: url(' + this.loadingImage + ');'
+      }
+
+      if (this.imageState === 'error') {
+        return 'background-image: url(' + this.errorImage + ');'
+      }
+
+      if (this.imageState === 'loaded') {
+        return 'background-image: url(' + this.asyncImage.src + '); background-size: ' + this.backgroundSize
+      }
+
+      return '';
+    }
+  },
+  methods: {
+    fetchImage (url) {
+      this.asyncImage.onload = this.imageOnLoad
+      this.asyncImage.onerror = this.imageOnError
+      this.imageState = 'loading'
+      this.asyncImage.src = this.imageSource
+    },
+    imageOnLoad (success) {
+      this.imageState = 'loaded'
+      this.imageWidth = this.asyncImage.naturalWidth
+      this.imageHeight = this.asyncImage.naturalHeight
+      this.imageSuccessCallback()
+    },
+    imageOnError () {
+      this.imageState = 'error'
+      this.imageErrorCallback()
+    }
+  },
+  mounted () {
+    var that = this;
+
+    this.$nextTick(() => {
+      window.setTimeout(function () {
+        that.fetchImage();
+      }, this.delay);
+    })
+  }
+}
+</script>
